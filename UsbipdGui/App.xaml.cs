0afa36d78aca26c41e2ddd510afdd580c42715c4
ryochack <ryochack@gmail.com>
2024-03-04 23:18:08 +0900

    First commit

---
+++ UsbipdGui/App.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using System.Windows.Forms;
+using static UsbipdGui.Usbipd;
+
+namespace UsbipdGui
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+        private System.Windows.Forms.NotifyIcon _notifyIcon = new();
+        private System.Windows.Forms.ContextMenuStrip _contextMenu = new();
+        private Usbipd? _usbipd = null;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            _usbipd = Usbipd.BuildUsbIpdCommnad();
+            if (_usbipd is null)
+            {
+                if (System.Windows.Forms.MessageBox.Show(
+                    "'usbipd-win' is not installed.\nWould you like to visit the installation page?",
+                    "usbipd-gui",
+                    System.Windows.Forms.MessageBoxButtons.YesNo,
+                    System.Windows.Forms.MessageBoxIcon.Asterisk
+                    ) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    System.Diagnostics.Process.Start(
+                        new System.Diagnostics.ProcessStartInfo()
+                        {
+                            FileName = "https://github.com/dorssel/usbipd-win/releases",
+                            UseShellExecute = true,
+                        }
+                    );
+                }
+                Shutdown();
+                return;
+            }
+
+            _notifyIcon.Visible = true;
+            _notifyIcon.Text = "usbipd";
+            _notifyIcon.Icon = new System.Drawing.Icon(GetResourceStream(new Uri("resource/usbip_darktheme.ico", UriKind.Relative)).Stream);
+            _notifyIcon.ContextMenuStrip = _contextMenu;
+            _notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(ClickNotifyIcon);
+
+            var usbDevices = _usbipd.GetUsbDevices();
+            UpdateContextMenu(ref _contextMenu, ref usbDevices);
+        }
+
+        private void UpdateContextMenu(ref System.Windows.Forms.ContextMenuStrip contextMenu, ref List<UsbDevice> usbDevices)
+        {
+            contextMenu.Items.Clear();
+            contextMenu.Text = "hogehoge";
+            List<UsbDevice> persistedDevices = [];
+
+            // TODO: ConnectedDevice のタイトルを入れたい
+            foreach (UsbDevice dev in usbDevices)
+            {
+                System.Diagnostics.Debug.WriteLine(dev);
+                switch (dev.State)
+                {
+                    case UsbDevice.ConnectionStates.DisconnectedPersisted:
+                        persistedDevices.Add(dev);
+                        break;
+                    case UsbDevice.ConnectionStates.ConnectedNotShared:
+                        contextMenu.Items.Add($"[{dev.BusId}] {dev.Vid}:{dev.Pid} {dev.Description}", null, ClickBindedDevice);
+                        ((ToolStripMenuItem)contextMenu.Items[contextMenu.Items.Count - 1]).Tag = dev;
+                        break;
+                    case UsbDevice.ConnectionStates.ConnectedShared:
+                        contextMenu.Items.Add($"[{dev.BusId}] {dev.Vid}:{dev.Pid} {dev.Description}", null, ClickUnbindedDevice);
+                        ((ToolStripMenuItem)contextMenu.Items[contextMenu.Items.Count - 1]).Checked = true;
+                        ((ToolStripMenuItem)contextMenu.Items[contextMenu.Items.Count - 1]).Tag = dev;
+                        break;
+                    case UsbDevice.ConnectionStates.ConnectedAttached:
+                        contextMenu.Items.Add($"[{dev.BusId}] {dev.Vid}:{dev.Pid} {dev.Description}");
+                        ((ToolStripMenuItem)contextMenu.Items[contextMenu.Items.Count - 1]).Checked = true;
+                        ((ToolStripMenuItem)contextMenu.Items[contextMenu.Items.Count - 1]).Tag = dev;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            // TODO: PersisteDevice のタイトルを入れたい
+            foreach (UsbDevice dev in persistedDevices)
+            {
+                contextMenu.Items.Add($"[   ] {dev.Vid}:{dev.Pid} {dev.Description}");
+                contextMenu.Items[contextMenu.Items.Count - 1].Enabled = false;
+            }
+        }
+
+        private void ClickNotifyIcon(object? sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Right:
+                    var usbDevices = _usbipd.GetUsbDevices();
+                    UpdateContextMenu(ref _contextMenu, ref usbDevices);
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void ClickBindedDevice(object? sender, EventArgs e)
+        {
+            UsbDevice device = (UsbDevice)((ToolStripMenuItem)sender).Tag;
+            System.Diagnostics.Debug.WriteLine($"usbipd bind {device.BusId}");
+        }
+
+        private void ClickUnbindedDevice(object? sender, EventArgs e)
+        {
+            UsbDevice device = (UsbDevice)((ToolStripMenuItem)sender).Tag;
+            System.Diagnostics.Debug.WriteLine($"usbipd unbind {device.BusId}");
+        }
+
+    }
+
+}
---
+++ UsbipdGui/Usbipd.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UsbipdGui
+{
+    partial class Usbipd
+    {
+        public readonly struct UsbDevice
+        {
+            public enum ConnectionStates
+            {
+                None = 0b_0000,
+                Connected = 0b_0001,
+                Shared = 0b_0010,
+                Attached = 0b_0100,
+                DisconnectedPersisted = Shared,
+                ConnectedNotShared = Connected,
+                ConnectedShared = Connected | Shared,
+                ConnectedAttached = Connected | Shared | Attached,
+                Unknown = 0b_1111,
+            }
+
+            public UsbDevice(string? busId, string? clientIpAddr, string? description, string? vid, string? pid, bool isForced, string? persistedGuid, string? stubInstanceId)
+            {
+                BusId = busId;
+                ClientIpAddr = clientIpAddr;
+                Description = description;
+                Vid = vid;
+                Pid = pid;
+                IsForced = isForced;
+                PersistedGuid = persistedGuid;
+                StubInstanceId = stubInstanceId;
+                State = (!String.IsNullOrWhiteSpace(BusId) ? ConnectionStates.Connected : ConnectionStates.None)
+                    | (!String.IsNullOrWhiteSpace(PersistedGuid) ? ConnectionStates.Shared : ConnectionStates.None)
+                    | (!String.IsNullOrWhiteSpace(ClientIpAddr) ? ConnectionStates.Attached : ConnectionStates.None);
+            }
+
+            public ConnectionStates State { get; init; }
+
+            public string? BusId { get; init; }
+            public string? ClientIpAddr { get; init; }
+            public string? Description { get; init; }
+            public string? Vid { get; init; }
+            public string? Pid { get; init; }
+            public bool IsForced { get; init; }
+            public string? PersistedGuid { get; init; }
+            public string? StubInstanceId { get; init; }
+
+            public override string ToString()
+            {
+                return $"BusId:{BusId}  Description:{Description}\n- State:{State}\n- ClientIpAddr:{ClientIpAddr}\n- VID/PID:{Vid}:{Pid}\n- IsForced:{IsForced}\n- PersistedGuid:{PersistedGuid}\n- StubInstanceId:{StubInstanceId}";
+            }
+        }
+
+        public static Usbipd? BuildUsbIpdCommnad()
+        {
+            if (IsExistsUsbipdCommand())
+            {
+                return new Usbipd();
+            }
+            else
+            {
+                return null;
+            }
+        }
+        private Usbipd() { }
+
+        public List<UsbDevice> GetUsbDevices()
+        {
+            List<UsbDevice> usbDevices = [];
+
+            System.Text.Json.JsonDocument? json = GetUsbIpdListAsJson();
+            if (json is null)
+            {
+                return usbDevices;
+            }
+
+            foreach (JsonElement device in json.RootElement.GetProperty("Devices").EnumerateArray())
+            {
+                (string vid, string pid) = ExtractUsbIds(device.GetProperty("InstanceId").GetString());
+                usbDevices.Add(new UsbDevice(
+                    device.GetProperty("BusId").GetString(),
+                    device.GetProperty("ClientIPAddress").GetString(),
+                    device.GetProperty("Description").GetString(),
+                    vid, pid,
+                    device.GetProperty("IsForced").GetBoolean(),
+                    device.GetProperty("PersistedGuid").GetString(),
+                    device.GetProperty("StubInstanceId").GetString()));
+            }
+            return usbDevices;
+        }
+
+        public bool Bind(ref UsbDevice device)
+        {
+            if (ExecuteCommand("usbpid", $"bind -f -b {device.BusId}") is null)
+            {
+                return false;
+            }
+
+            List<UsbDevice> updatedDevices = GetUsbDevices();
+            if (updatedDevices.Count == 0)
+            {
+                return false;
+            }
+            foreach (UsbDevice udev in updatedDevices)
+            {
+                if (device.BusId == udev.BusId)
+                {
+                    if ((udev.State & UsbDevice.ConnectionStates.Shared) == UsbDevice.ConnectionStates.Shared)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool Unbind(ref UsbDevice device)
+        {
+            if (ExecuteCommand("usbpid", $"unbind -b {device.BusId}") is null)
+            {
+                return false;
+            }
+
+            List<UsbDevice> updatedDevices = GetUsbDevices();
+            if (updatedDevices.Count == 0)
+            {
+                return false;
+            }
+            foreach (UsbDevice udev in updatedDevices)
+            {
+                if (device.BusId == udev.BusId)
+                {
+                    if ((udev.State & UsbDevice.ConnectionStates.Shared) == UsbDevice.ConnectionStates.None)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private static string? ExecuteCommand(string command, string args)
+        {
+            try
+            {
+                using Process proc = new();
+                proc.StartInfo.FileName = command;
+                proc.StartInfo.Arguments = args;
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.StandardOutputEncoding = Encoding.UTF8;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.Start();
+                string result = proc.StandardOutput.ReadToEnd();
+                proc.WaitForExit();
+                return result;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private static bool IsExistsUsbipdCommand()
+        {
+            return !string.IsNullOrWhiteSpace(ExecuteCommand("where", "usbipd"));
+        }
+
+        private System.Text.Json.JsonDocument? GetUsbIpdListAsJson()
+        {
+            string? jsonString = ExecuteCommand("usbipd", "state");
+            if (string.IsNullOrWhiteSpace(jsonString))
+            {
+                return null;
+            }
+
+            try
+            {
+                System.Text.Json.JsonDocument json = System.Text.Json.JsonDocument.Parse(jsonString);
+                return json;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        // Regex target examples:
+        // - "InstanceId: USB\VID_8087 & PID_0025\7 & 2E104BF0 & 0 & 2"
+        // - "InstanceId: USB\VID_0403 & PID_6001\A901O7VP"
+        [GeneratedRegex(@"USB\\VID_(\w{4})&PID_(\w{4})")]
+        private static partial Regex MyRegex();
+
+        private static (string vid, string pid) ExtractUsbIds(string? instanceIdString)
+        {
+            if (string.IsNullOrWhiteSpace(instanceIdString))
+            {
+                return ("-", "-");
+            }
+            Regex regex = MyRegex();
+
+            Match match = regex.Match(instanceIdString);
+            if (!match.Success)
+            {
+                return ("-", "-");
+            }
+            return (match.Groups[1].Value, match.Groups[2].Value);
+        }
+
+    }
+}
